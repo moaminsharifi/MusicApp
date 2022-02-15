755675971780d93a1e272aaac48017a109830135
Amin Sharifi <MoAminSharifiMinabi@gmail.com>
2022-02-15 19:22:58 +0330

    Login and Singup FormRequest file  for API AuthController added

---
+++ backend/app/Http/Requests/API/AuthController/SingUpRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests\API\AuthController;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class SingUpRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'required|string|max:180',
+            'email' => 'required|string|email|unique:users|max:180',
+            'password' => 'required|string|max:180',
+            'password_confirm'=> 'required|same:password|max:180'
+        ];
+    }
+}
