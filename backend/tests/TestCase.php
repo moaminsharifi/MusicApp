38051ef898c65a8b7359962b74518e1a2a3564d1
Amin Sharifi <MoAminSharifiMinabi@gmail.com>
2022-02-15 18:06:59 +0330

    due to using in memory and memory leak problem use each time migrate for security reason

--- backend/tests/TestCase.php
+++ backend/tests/TestCase.php
@@ -3,8 +3,25 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Support\Facades\Artisan;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        Artisan::call('migrate');
+        #Artisan::call('passport:install');
+    }
+    public function tearDown(): void
+    {
+
+
+        Artisan::call('migrate:fresh');
+        parent::tearDown();
+    }
+
+
 }
