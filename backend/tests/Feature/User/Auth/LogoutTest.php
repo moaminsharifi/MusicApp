15e5eb9da22ae538e1d680f56ac94792290adbcc
Amin Sharifi <MoAminSharifiMinabi@gmail.com>
2022-02-18 16:23:00 +0330

    success case and invalid token case added to  Logout

--- backend/tests/Feature/User/Auth/LogoutTest.php
+++ backend/tests/Feature/User/Auth/LogoutTest.php
@@ -5,18 +5,122 @@ namespace Tests\Feature\User\Auth;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
-
+use Database\Factories\UserAPIFactory;
+use Illuminate\Http\Response;
+use App\Models\User;
 class Logout extends TestCase
 {
-    /**
-     * A basic feature test example.
-     *
+    use RefreshDatabase;
+    private $jsonStructUserData =  [
+            'ok',
+            'data'=>[
+                'token',
+                'token_type',
+                'is_admin',
+                'name',
+                'email'
+            ]
+    ];
+
+     /**
+     * A test for successfully logout
+     * @test
+     * @group user
+     * @group user.logout
      * @return void
      */
-    public function test_example()
-    {
-        $response = $this->get('/');
+    public function success(){
+        // prepare data
+         $user = User::factory()->create();
+         $data = [
+            'email' =>$user->email,
+            'password'=>'password'];
+        // before send assertion
 
-        $response->assertStatus(200);
+        // send request
+        $response = $this->json('POST' , route('api.login'), $data);
+
+        // after send request assertion
+        $response->assertStatus(Response::HTTP_OK)
+            ->assertJsonStructure(
+                $this->jsonStructUserData
+        );
+
+        // prepare data
+        $token = json_decode($response->getContent())->data->token;
+        $header = [
+            'HTTP_Authorization' => 'Bearer ' . $token,
+            'Accept' => 'application/json',
+            'Content-Type' => 'application/json',
+        ];
+
+        // before send assertion
+
+        // send request
+        $response = $this->json('GET',route('api.logout'), [], $header);
+        // after send request assertion
+        $response->assertStatus(200)
+        ->assertJsonStructure(
+            [
+                'ok',
+                'data'
+            ]
+        );
+
     }
+    /**
+     * A test for invalid_token
+     * @test
+     * @group user
+     * @group user.logout
+     * @return void
+     */
+    public function invalid_token(){
+        // prepare data
+         $user = User::factory()->create();
+         $data = [
+            'email' =>$user->email,
+            'password'=>'password'];
+        // before send assertion
+
+        // send request
+        $response = $this->json('POST' , route('api.login'), $data);
+
+        // after send request assertion
+        $response->assertStatus(Response::HTTP_OK)
+            ->assertJsonStructure(
+                $this->jsonStructUserData
+        );
+
+        // prepare data
+        $token = 'RANDOM_TOKEN';
+        $header = [
+            'HTTP_Authorization' => 'Bearer ' . $token,
+            'Accept' => 'application/json',
+            'Content-Type' => 'application/json',
+        ];
+
+        // before send assertion
+
+        // send request
+        $response = $this->json('GET',route('api.logout'), [], $header);
+        // after send request assertion
+        $response->assertStatus(Response::HTTP_UNAUTHORIZED)->assertJson([
+            'ok'=>0,
+            'error'=>[
+                'code'=>"00005",
+                'message'=>"Unauthenticated.",
+            ],
+            'data'=>[
+
+            ]
+            ]);
+
+
+
+    }
+
+
+
+
 }
