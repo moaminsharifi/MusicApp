b7f9df3feb4f641ac35989505ac971c0ff8c9993
Amin Sharifi <MoAminSharifiMinabi@gmail.com>
2022-02-18 16:23:13 +0330

    success case added to test

--- backend/tests/Feature/User/Auth/UserDataTest.php
+++ backend/tests/Feature/User/Auth/UserDataTest.php
@@ -5,18 +5,66 @@ namespace Tests\Feature\User\Auth;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use Database\Factories\UserAPIFactory;
+use Illuminate\Http\Response;
+use App\Models\User;
 
 class UserData extends TestCase
 {
+    use RefreshDatabase;
+    use WithFaker;
+    private $jsonStructUserData =  [
+            'ok',
+            'data'=>[
+                'token',
+                'token_type',
+                'is_admin',
+                'name',
+                'email'
+            ]
+    ];
     /**
-     * A basic feature test example.
-     *
+     * A test for successfully getUserData
+     * @test
+     * @group user
+     * @group user.data
      * @return void
      */
-    public function test_example()
-    {
-        $response = $this->get('/');
+    public function success(){
+        // prepare data
+         $user = User::factory()->create();
+         $data = [
+            'email' =>$user->email,
+            'password'=>'password'];
+        // before send assertion
 
-        $response->assertStatus(200);
+        // send request
+        $response = $this->json('POST' , route('api.login'), $data);
+
+        // after send request assertion
+        $response->assertStatus(Response::HTTP_OK)
+            ->assertJsonStructure(
+                $this->jsonStructUserData
+        );
+
+        // prepare data
+        $token = json_decode($response->getContent())->data->token;
+        $header = [
+            'HTTP_Authorization' => 'Bearer ' . $token,
+            'Accept' => 'application/json',
+            'Content-Type' => 'application/json',
+        ];
+
+        // before send assertion
+
+        // send request
+        $response = $this->json('GET',route('api.userData'), [], $header);
+        print_r($response->getContent());
+        // after send request assertion
+        $response->assertStatus(Response::HTTP_OK)
+            ->assertJsonStructure(
+                $this->jsonStructUserData
+        );
+
     }
 }
